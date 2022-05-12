1f60d1dc43308035989874713a37ca42005990ce
Soliman Shady <89577693+SolimanShady@users.noreply.github.com>
2022-05-12 13:53:36 +0200

    Update smtp.php

--- config/smtp.php
+++ config/smtp.php
@@ -1,7 +1,7 @@
 <?php return array (
-  'smtp_server' => 'smtp.gmail.com',
-  'smtp_username' => '‬',
-  'smtp_password' => '',
-  'smtp_secure' => 'tls',
-  'smtp_port' => '587',
+  'smtp_server' => "smtp.gmail.com",
+  'smtp_username' => "",
+  'smtp_password' => "",
+  'smtp_secure' => "tls",
+  'smtp_port' => "587",
 ); ?>
